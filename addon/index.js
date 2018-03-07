a1c5e3ced7092b9ec1564f2856a9fb8cefaa0d1c
Robert Jackson <rjackson@linkedin.com>
2018-03-07 10:02:16 -0500

    Use ember-compatibility-helpers to avoid double inclusion.
    Using ember-compatibility-helpers' `gte` allows us to avoid shipping any
    additional code in versions of Ember that already include
    `Ember.OrderedSet`.

--- addon/index.js
+++ addon/index.js
@@ -1,153 +1,160 @@
+import { gte } from 'ember-compatibility-helpers';
 import { guidFor } from '@ember/object/internals';
 import { assert } from '@ember/debug';
+import Ember from 'ember';
 
-/**
+const NEEDS_CUSTOM_ORDERED_SET = gte('3.5.0-alpha.1');
+
+let OrderedSet;
+
+if (NEEDS_CUSTOM_ORDERED_SET) {
+  /**
   @class OrderedSet
   @constructor
-*/
-export default class OrderedSet {
-  constructor() {
-    this.clear();
-  }
+  */
+  OrderedSet = class OrderedSet {
+    constructor() {
+      this.clear();
+    }
 
-  /**
+    /**
     @method create
     @static
     @return {OrderedSet}
-  */
-  static create() {
-    let Constructor = this;
-    return new Constructor();
-  }
+    */
+    static create() {
+      let Constructor = this;
+      return new Constructor();
+    }
 
-  /**
+    /**
     @method clear
-  */
-  clear() {
-    this.presenceSet = Object.create(null);
-    this.list = [];
-    this.size = 0;
-  }
+    */
+    clear() {
+      this.presenceSet = Object.create(null);
+      this.list = [];
+      this.size = 0;
+    }
 
-  /**
+    /**
     @method add
     @param {*} obj
     @param {string} [_guid] (for internal use)
     @return {OrderedSet}
-  */
-  add(obj, _guid) {
-    let guid = _guid || guidFor(obj);
-    let presenceSet = this.presenceSet;
-    let list = this.list;
+    */
+    add(obj, _guid) {
+      let guid = _guid || guidFor(obj);
+      let presenceSet = this.presenceSet;
+      let list = this.list;
 
-    if (presenceSet[guid] !== true) {
-      presenceSet[guid] = true;
-      this.size = list.push(obj);
+      if (presenceSet[guid] !== true) {
+        presenceSet[guid] = true;
+        this.size = list.push(obj);
+      }
+
+      return this;
     }
 
-    return this;
-  }
-
-  /**
+    /**
     @method delete
     @param {*} obj
     @param {string} [_guid] (for internal use)
     @return {Boolean}
-  */
-  delete(obj, _guid) {
-    let guid = _guid || guidFor(obj);
-    let presenceSet = this.presenceSet;
-    let list = this.list;
+    */
+    delete(obj, _guid) {
+      let guid = _guid || guidFor(obj);
+      let presenceSet = this.presenceSet;
+      let list = this.list;
 
-    if (presenceSet[guid] === true) {
-      delete presenceSet[guid];
-      let index = list.indexOf(obj);
-      if (index > -1) {
-        list.splice(index, 1);
+      if (presenceSet[guid] === true) {
+        delete presenceSet[guid];
+        let index = list.indexOf(obj);
+        if (index > -1) {
+          list.splice(index, 1);
+        }
+        this.size = list.length;
+        return true;
+      } else {
+        return false;
       }
-      this.size = list.length;
-      return true;
-    } else {
-      return false;
     }
-  }
 
-  /**
+    /**
     @method isEmpty
     @return {Boolean}
-  */
-  isEmpty() {
-    return this.size === 0;
-  }
+    */
+    isEmpty() {
+      return this.size === 0;
+    }
 
-  /**
+    /**
     @method has
     @param {*} obj
     @return {Boolean}
-  */
-  has(obj) {
-    if (this.size === 0) { return false; }
+    */
+    has(obj) {
+      if (this.size === 0) { return false; }
 
-    let guid = guidFor(obj);
-    let presenceSet = this.presenceSet;
+      let guid = guidFor(obj);
+      let presenceSet = this.presenceSet;
 
-    return presenceSet[guid] === true;
-  }
+      return presenceSet[guid] === true;
+    }
 
-  /**
+    /**
     @method forEach
     @param {Function} fn
     @param self
-  */
-  forEach(fn /*, ...thisArg*/) {
-    assert(`${Object.prototype.toString.call(fn)} is not a function`, typeof fn === 'function')
+    */
+    forEach(fn /*, ...thisArg*/) {
+      assert(`${Object.prototype.toString.call(fn)} is not a function`, typeof fn === 'function')
 
-    if (this.size === 0) { return; }
+      if (this.size === 0) { return; }
 
-    let list = this.list;
+      let list = this.list;
 
-    if (arguments.length === 2) {
-      for (let i = 0; i < list.length; i++) {
-        fn.call(arguments[1], list[i]);
-      }
-    } else {
-      for (let i = 0; i < list.length; i++) {
-        fn(list[i]);
+      if (arguments.length === 2) {
+        for (let i = 0; i < list.length; i++) {
+          fn.call(arguments[1], list[i]);
+        }
+      } else {
+        for (let i = 0; i < list.length; i++) {
+          fn(list[i]);
+        }
       }
     }
-  }
 
-  /**
+    /**
     @method toArray
     @return {Array}
-  */
-  toArray() {
-    return this.list.slice();
-  }
+    */
+    toArray() {
+      return this.list.slice();
+    }
 
-  /**
+    /**
     @method copy
     @return {OrderedSet}
-  */
-  copy() {
-    let Constructor = this.constructor;
-    let set = new Constructor();
+    */
+    copy() {
+      let Constructor = this.constructor;
+      let set = new Constructor();
 
-    set.presenceSet = copyNull(this.presenceSet);
-    set.list = this.toArray();
-    set.size = this.size;
+      set.presenceSet = Object.create(null);
 
-    return set;
+      for (let prop in this.presenceSet) {
+        // hasOwnPropery is not needed because obj is Object.create(null);
+        set.presenceSet[prop] = this.presenceSet[prop];
+      }
+
+      set.list = this.toArray();
+      set.size = this.size;
+
+      return set;
+    }
   }
+} else {
+  OrderedSet = Ember.OrderedSet;
 }
 
-function copyNull(obj) {
-  let output = Object.create(null);
-
-  for (let prop in obj) {
-    // hasOwnPropery is not needed because obj is Object.create(null);
-    output[prop] = obj[prop];
-  }
-
-  return output;
-}
+export default OrderedSet;
